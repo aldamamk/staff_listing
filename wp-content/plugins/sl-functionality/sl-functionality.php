8b3b45a5cd56db5566f9da589f3344ca5d8fd420
krizzel <72677658+aldamamk@users.noreply.github.com>
2021-04-27 13:59:14 -0700

    created a functionality plugin

---
+++ wp-content/plugins/sl-functionality/sl-functionality.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name:       Functionality Plugin
+ * Plugin URI:        http://example.com/plugin-name-uri/
+ * Description:       all functionality in this plugin will go to any theme you choose
+ * Version:           1.0.0
+ * Author:            krizzel
+ */
