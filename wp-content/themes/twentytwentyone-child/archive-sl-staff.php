1908cfa2cd5f80a1b3556bce708cc6ab2c466ba9
krizzel <72677658+aldamamk@users.noreply.github.com>
2021-04-27 18:23:35 -0700

    fixed html semantics

--- wp-content/themes/twentytwentyone-child/archive-sl-staff.php
+++ wp-content/themes/twentytwentyone-child/archive-sl-staff.php
@@ -9,55 +9,47 @@
 
 get_header();
 ?>
+    <section class="banner-section">
+        <div class="banner-image staff-banner"></div>
+        <h1> Meet Our Team </h1>
+    </section>
+    
+    <div class="flex">
 
-	<main id="primary" class="site-main">
+        <?php
+            $taxonomy = 'sl-staff-categories';
+            $terms = get_terms(
+                array(
+                    'taxonomy' => $taxonomy
+                )
+            );
+            if ( $terms && !is_wp_error($terms) ) : 
+                foreach ( $terms as $term ) :
+                    $term_args = array(
+                        'post_type'      => 'sl-staff',
+                        'posts_per_page' => -1,
+                        'tax_query'      => array(
+                            array(
+                                'taxonomy' => $taxonomy,
+                                'field'    => 'slug',
+                                'terms'    => $term->slug,
+                            )
+                        ),
+                    );
+                    $term_query = new WP_Query( $term_args );
+                if ( $term_query->have_posts() ) : ?>
 
-        <section class="banner-section">
-            <div class="banner-image staff-banner"></div>
-            <h1> Meet Our Team </h1>
-        </section>
-       
-
-        
-        <div class="flex-section">
-
-    <?php
-        $taxonomy = 'sl-staff-categories';
-        $terms = get_terms(
-            array(
-                'taxonomy' => $taxonomy
-            )
-        );
-        if ( $terms && !is_wp_error($terms) ) : 
-            foreach ( $terms as $term ) :
-                $term_args = array(
-                    'post_type'      => 'sl-staff',
-                    'posts_per_page' => -1,
-                    'tax_query'      => array(
-                        array(
-                            'taxonomy' => $taxonomy,
-                            'field'    => 'slug',
-                            'terms'    => $term->slug,
-                        )
-                    ),
-                );
-                $term_query = new WP_Query( $term_args );
-            if ( $term_query->have_posts() ) : ?>
-
-                    <section class="staff-section">
-                        <a href="<?php echo get_term_link($term) ?>">
-                            <h2><?php echo $term->name; ?></h2>
-                        </a>
-                    </section>
-               
-                    <?php wp_reset_postdata();
-                endif;
-            endforeach;
-        endif;
-?>
+                        <section class="staff-section">
+                            <a href="<?php echo get_term_link($term) ?>">
+                                <h2><?php echo $term->name; ?></h2>
+                            </a>
+                        </section>
+                
+                        <?php wp_reset_postdata();
+                    endif;
+                endforeach;
+            endif;
+        ?>
      </div>
-
-	</main><!-- #main -->
-
 <?php
 get_footer();
--- wp-content/themes/twentytwentyone-child/taxonomy-sl-staff-categories-san-francisco.php
+++ wp-content/themes/twentytwentyone-child/taxonomy-sl-staff-categories-san-francisco.php
@@ -9,44 +9,39 @@
 
 get_header();
 ?>
-<div class="wrapper-staff-cat">
-        <main id="primary" class="site-main">
+    <div class="wrapper-staff-cat">
+    
+        <?php   if ( have_posts() ) : ?>
 
-            <?php   if ( have_posts() ) : ?>
+            <section class="banner-section">
+                <div class="banner-image"></div>
+                <h1>Our <?php single_term_title();?> Team</h1>
+            </section>
 
-                <section class="banner-section">
-                    <div class="banner-image"></div>
-                    <h1>Our <?php single_term_title();?> Team</h1>
-                </section>
+            <section class="staff-list">   
+                <?php while(have_posts() ) :
+                    the_post() ?>
 
-                <section class="staff-list">   
-                    <?php while(have_posts() ) :
-                        the_post() ?>
+                    <article class="staff-single-card">
 
-                        <article class="staff-single-card">
+                    <?php  the_post_thumbnail(); ?>
 
-                        <?php  the_post_thumbnail(); ?>
+                        <h2> <?php the_title(); ?> </h2>
+                            <p> <?php    
+                                $position = get_field('position');
+                                if($position) : 
+                                echo $position; 
+                                endif; ?> 
+                            </p>
+                    </article>
 
-                           <h2> <?php the_title(); ?> </h2>
-                                <p> <?php    
-                                    $position = get_field('position');
-                                    if($position) : 
-                                    echo $position; 
-                                    endif; ?> 
-                                </p>
-                        </article>
-
-                        <?php
-                            endwhile; 
-                            
-                            wp_reset_postdata();
-                    
-                    endif;?>
-                </section>
-                    
-                    
-
-        </main><!-- #main -->
+                    <?php
+                        endwhile; 
+                        
+                        wp_reset_postdata();
+                
+                endif;?>
+            </section>
     </div>
 <?php
 get_footer();
