88a4d0692cad553dc2a3dd0830cc090792008b24
krizzel <72677658+aldamamk@users.noreply.github.com>
2021-04-27 17:06:21 -0700

    display staff taxonomy to 'our team' page

---
+++ wp-content/themes/twentytwentyone-child/archive-sl-staff.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * The template for displaying archive staff pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package School_TWD_theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+        <section class="banner-section">
+            <div class="banner-image staff-banner"></div>
+            <h1> Meet Our Team </h1>
+        </section>
+       
+
+        
+        <div class="flex-section">
+
+    <?php
+        $taxonomy = 'sl-staff-categories';
+        $terms = get_terms(
+            array(
+                'taxonomy' => $taxonomy
+            )
+        );
+        if ( $terms && !is_wp_error($terms) ) : 
+            foreach ( $terms as $term ) :
+                $term_args = array(
+                    'post_type'      => 'sl-staff',
+                    'posts_per_page' => -1,
+                    'tax_query'      => array(
+                        array(
+                            'taxonomy' => $taxonomy,
+                            'field'    => 'slug',
+                            'terms'    => $term->slug,
+                        )
+                    ),
+                );
+                $term_query = new WP_Query( $term_args );
+            if ( $term_query->have_posts() ) : ?>
+
+                    <section class="staff-section">
+                        <a href="<?php echo get_term_link($term) ?>">
+                            <h2><?php echo $term->name; ?></h2>
+                        </a>
+                    </section>
+               
+                    <?php wp_reset_postdata();
+                endif;
+            endforeach;
+        endif;
+?>
+     </div>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
