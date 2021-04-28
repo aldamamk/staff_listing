3a60c8dc273d822265973ea290860207f8273348
krizzel <72677658+aldamamk@users.noreply.github.com>
2021-04-27 17:22:21 -0700

    create page for staff categories

---
+++ wp-content/themes/twentytwentyone-child/taxonomy-sl-staff-categories-san-francisco.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The template for displaying student designers pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package School_TWD_theme
+ */
+
+get_header();
+?>
+<div class="wrapper-staff-cat">
+        <main id="primary" class="site-main">
+
+            <?php   if ( have_posts() ) : ?>
+
+                <section class="banner-section">
+                    <div class="banner-image"></div>
+                    <h1>Our <?php single_term_title();?> Team</h1>
+                </section>
+
+                <section class="staff-list">   
+                    <?php while(have_posts() ) :
+                        the_post() ?>
+
+                        <article class="staff-single-card">
+
+                        <?php  the_post_thumbnail(); ?>
+
+                           <h2> <?php the_title(); ?> </h2>
+                                <p> <?php    
+                                    $position = get_field('position');
+                                    if($position) : 
+                                    echo $position; 
+                                    endif; ?> 
+                                </p>
+                        </article>
+
+                        <?php
+                            endwhile; 
+                            
+                            wp_reset_postdata();
+                    
+                    endif;?>
+                </section>
+                    
+                    
+
+        </main><!-- #main -->
+    </div>
+<?php
+get_footer();
